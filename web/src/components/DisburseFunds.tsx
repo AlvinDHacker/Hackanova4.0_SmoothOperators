70d08b811194f48caf250d1218ae075838c7496c
AlvinDHacker <alvindsouza2204@gmail.com>
2025-02-22 11:03:27 +0530

    Micro Changes Demo

--- web/src/components/DisburseFunds.tsx
+++ web/src/components/DisburseFunds.tsx
@@ -13,6 +13,7 @@ import {
   DialogTrigger,
 } from "~/components/ui/dialog";
 import { Separator } from "./ui/separator";
+import { Textarea } from "./ui/textarea";
 
 const DisburseFunds = () => {
   const img = `https://cdn.simpleicons.org/ethereum/ethereum`;
@@ -32,27 +33,23 @@ const DisburseFunds = () => {
           </DialogHeader>
           <div className="grid gap-4 py-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Full Name</Label>
-              <Input id="name" placeholder="Full Name here" />
+              <Label htmlFor="name">Amount Required</Label>
+              <Input id="name" placeholder="Amount in ETH" />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Phone No.</Label>
-              <Input id="name" placeholder="+91" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Aadhar Card No.</Label>
+              <Label htmlFor="name">Fields of Assist</Label>
               <Input id="name" />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">GST No.</Label>
-              <Input id="name" />
+              <Label htmlFor="name">Assisting Description</Label>
+              <Textarea id="name" rows={3} />
             </div>
           </div>
           <Separator />
           <DialogFooter>
             <Button variant={"outline"} type="submit">
               <img src={img} height={15} width={15} alt="Hi" />
-              Login with Ethereum
+              Request Funds in Ethereum
             </Button>
           </DialogFooter>
         </DialogContent>
